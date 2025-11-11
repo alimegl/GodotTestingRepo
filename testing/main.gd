7134da8c295be0c09fd8a420ed81dd76476f8883
alimegl <alim.erguel@volkswagen.de>
2025-11-11 14:49:17 +0100

    finished at "The main game scene"

--- testing/main.gd
+++ testing/main.gd
@@ -6,21 +6,20 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	$HUD.show_game_over()
-	$Music.stop()
-	$DeathSound.play()
-
 
 func new_game():
-	get_tree().call_group(&"mobs", &"queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-	$Music.play()
+	
 
+func _on_score_timer_timeout():
+	score += 1
 
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
@@ -45,21 +44,6 @@ func _on_mob_timer_timeout():
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
-
-
-func _on_ScoreTimer_timeout():
-	score += 1
-	$HUD.update_score(score)
-
-
-func _on_StartTimer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
-
-
-func _on_start_timer_timeout() -> void:
-	pass # Replace with function body.
-
-
-func _on_score_timer_timeout() -> void:
-	pass # Replace with function body.
+	
+func _ready():
+	pass
